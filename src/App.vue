3e4347e299d1b8fc687e659923a5e1e242d29f32
ozgeterz <terziozge@hotmail.com>
2022-08-24 16:18:00 +0300

    Filtreleme işlemi eklendi

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,61 @@
 <template>
   <div id="app">
-    <JobList />
+    <JobList :jobList="filterBySearch"/>
   </div>
 </template>
 
 <script>
 import JobList from './components/JobList.vue';
+import { eventBus } from "@/main";
 export default {
-    components: { JobList }
+    components: { JobList },
+      data: () => ({ jobList:[{
+    name:"furniture",
+    svg:"../svg/car-back-side-seats-icon.svg"
+},
+{
+  name:"property",
+  svg:"../svg/analytics-icon.svg"
+},
+{
+  name:"travel",
+  svg:"../svg/analytics-icon.svg"
+},
+{
+  name:"boat",
+  svg:"../svg/analytics-icon.svg"
+},
+{
+  name:"mc",
+  svg:"../svg/analytics-icon.svg"
+},
+{
+  name:"job",
+  svg:"../svg/analytics-icon.svg"
+},
+{
+  name:"economy",
+  svg:"../svg/analytics-icon.svg"
+},
+{
+  name:"holiday",
+  svg:"../svg/analytics-icon.svg"
+}
+  ],
+  data:""
+   }),
+   methods:{ filter: function (obj) {
+      return obj.name.includes(this.data);
+    }}, 
+    created() {
+    eventBus.$on("search-query", (data) => {
+      this.data=data;
+    });
+    
+  },
+    computed:{filterBySearch:function(){
+    return this.jobList.filter(this.filter);
+    }}
 }
 </script>
 <style>
--- src/components/JobList.vue
+++ src/components/JobList.vue
@@ -1,65 +1,31 @@
 <template>
-
   <div class="search-container">
     <div class="reactangle-2"></div>
     <div class="content">
-      <SearchBar/>
+      <SearchBar />
       <div class="cards">
-          <CardsButton v-for="item in jobList" :key="item.name" :color="[item.name]" ><img :src="item.svg" >{{item.name}}</CardsButton>
+        <CardsButton
+          v-for="item in jobList"
+          :key="item.name"
+          :color="[item.name]"
+          ><img :src="item.svg" />{{ item.name }}</CardsButton
+        >
       </div>
-     </div>
+    </div>
   </div>
 </template>
 <script>
 import CardsButton from "./CardsButton.vue";
 import SearchBar from "./SearchBar.vue";
-import { eventBus } from "@/main";
+
 export default {
- name: "JobList",
+  name: "JobList",
   components: {
     CardsButton,
-    SearchBar
-}, 
-  data: () => ({ jobList:[{
-    name:"furniture",
-    svg:"../svg/car-back-side-seats-icon.svg"
-},
-{
-  name:"property",
-  svg:"../svg/analytics-icon.svg"
-},
-{
-  name:"travel",
-  svg:"../svg/analytics-icon.svg"
-},
-{
-  name:"boat",
-  svg:"../svg/analytics-icon.svg"
-},
-{
-  name:"mc",
-  svg:"../svg/analytics-icon.svg"
-},
-{
-  name:"job",
-  svg:"../svg/analytics-icon.svg"
-},
-{
-  name:"economy",
-  svg:"../svg/analytics-icon.svg"
-},
-{
-  name:"holiday",
-  svg:"../svg/analytics-icon.svg"
-}
-  ] }),
-  methods: {
-    },
-  created(){
-    eventBus.$on("search-query", (data)=>{
-      console.log(data) 
-
-     })}
+    SearchBar,
+  },
+  props: { jobList: { type: Array } },
+  methods: {},
 };
 </script>
 <style lang="scss" scoped>
@@ -73,14 +39,14 @@ export default {
   background: #f7f4ff;
   padding: 73px;
 }
- .reactangle-2 {
+.reactangle-2 {
   position: absolute;
   width: 421px;
   height: 613px;
   background: #efebfd;
   border-radius: 31px;
-} 
-.mc-btn{
+}
+.mc-btn {
   background: #ffffff;
   border: 1px solid #dbd5ec;
   box-shadow: 0px 14px 16px rgba(41, 11, 128, 0.1);
@@ -102,17 +68,17 @@ export default {
   display: grid;
   grid-template-columns: repeat(4, 1fr);
   grid-template-rows: repeat(2, 1fr);
-  grid-gap:16px;
+  grid-gap: 16px;
   width: 100%;
   height: 100%;
 }
 .svg {
-    padding-top: 20px;
-    width: 40px;
-    height: 40px;
+  padding-top: 20px;
+  width: 40px;
+  height: 40px;
 }
 .svg-search {
-    padding-right: 5px;
-    width: 20px;
+  padding-right: 5px;
+  width: 20px;
 }
 </style>
