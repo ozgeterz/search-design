93e5bc0f91ef0d99bdf31a1eb8700f2ef4b1b4ac
ozgeterz <terziozge@hotmail.com>
2022-08-25 12:30:43 +0300

    .

--- src/components/JobList.vue
+++ src/components/JobList.vue
@@ -1,27 +1,27 @@
 <template>
-  <div class="search-container">
-    <div class="reactangle-2"></div>
-    <div class="content">
+  <div class="search">
+    <div class="search-back"></div>
+    <div class="search-content">
       <SearchBar />
-      <div class="cards">
-        <CardsButton
+      <div class="search-cards">
+        <CardButton
           v-for="item in jobList"
           :key="item.name"
           :color="[item.name]"
-          ><img :src="item.svg" />{{ item.name }}</CardsButton
+          ><img :src="item.svg" />{{ item.name }}</CardButton
         >
       </div>
     </div>
   </div>
 </template>
 <script>
-import CardsButton from "./CardsButton.vue";
+import CardButton from "./CardButton.vue";
 import SearchBar from "./SearchBar.vue";
 
 export default {
   name: "JobList",
   components: {
-    CardsButton,
+    CardButton,
     SearchBar,
   },
   props: { jobList: { type: Array } },
@@ -29,7 +29,7 @@ export default {
 };
 </script>
 <style lang="scss" scoped>
-.search-container {
+.search {
   position: relative;
   width: 100%;
   height: 100vh;
@@ -38,20 +38,20 @@ export default {
   justify-content: center;
   background: #f7f4ff;
   padding: 73px;
-}
-.reactangle-2 {
+
+&-back {
   position: absolute;
   width: 421px;
   height: 613px;
   background: #efebfd;
   border-radius: 31px;
 }
-.mc-btn {
+&-mc-btn {
   background: #ffffff;
   border: 1px solid #dbd5ec;
   box-shadow: 0px 14px 16px rgba(41, 11, 128, 0.1);
 }
-.content {
+&-content {
   width: 640px;
   height: 464px;
   padding: 73px;
@@ -64,7 +64,7 @@ export default {
   justify-content: space-between;
   z-index: 1;
 }
-.cards {
+&-cards {
   display: grid;
   grid-template-columns: repeat(4, 1fr);
   grid-template-rows: repeat(2, 1fr);
@@ -72,13 +72,5 @@ export default {
   width: 100%;
   height: 100%;
 }
-.svg {
-  padding-top: 20px;
-  width: 40px;
-  height: 40px;
-}
-.svg-search {
-  padding-right: 5px;
-  width: 20px;
 }
 </style>
